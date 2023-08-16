35c74329575ce4eddb8368f16001833bf2b933d8
DianaKryzhanivska <136447767+DianaKryzhanivska@users.noreply.github.com>
2023-08-16 16:25:48 +0000

    Deploying to gh-pages from @ DianaKryzhanivska/goit-js-hw-09@6bea902941433eebb8f43efd318dac00f36db675 🚀

---
+++ 01-color-switcher.dfeb9bd6.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.setAttribute("disabled",!0)})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled")}));let e=null;
+//# sourceMappingURL=01-color-switcher.dfeb9bd6.js.map
